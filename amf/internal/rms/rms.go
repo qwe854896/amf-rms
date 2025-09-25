8b79958859dc6cda565668408458743759269ba8
Ian Chen <ychen.cs10@nycu.edu.tw>
2025-09-25 18:23:54 +0800

    chore: init commit

---
+++ amf/internal/rms/rms.go
@@ -0,0 +1,17 @@
+package rsm
+
+import "github.com/free5gc/util/fsm"
+
+type CustomizedRMS struct {
+	// implement your customized RMS fields here
+}
+
+func NewRMS(
+// implement your customized RMS initialization here
+) fsm.RMS {
+	return &CustomizedRMS{}
+}
+
+func (rms *CustomizedRMS) HandleEvent(state *fsm.State, event fsm.EventType, args fsm.ArgsType) {
+	// implement your customized RMS logic here
+}
